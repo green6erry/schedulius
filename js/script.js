60a68af85fa4a01a48f817e4bf913e9136b01c29
green6erry <green6erry@gmail.com>
2016-03-18 16:30:27 -0400

    Eh
    eh

---
+++ js/script.js
@@ -0,0 +1,24 @@
+ $(document).ready(function() {
+  alert('Oh, hi there, world');
+
+
+
+$("#menu > )
+
+ $(function() {
+    $( "#menu" ).menu();
+  });
+ 
+ 
+$(document).find(".ui-menu").css("width, 150px");
+
+
+
+
+
+
+
+
+
+
+});
